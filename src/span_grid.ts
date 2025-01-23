00f06fd3fb4b71d173b19ea401fff1305883e35d
Lorenzo Botti <lorenzobotti@fattureincloud.it>
2025-01-23 11:56:43 +0100

    feat: make `SpanGrid.container` nullable

--- src/span_grid.ts
+++ src/span_grid.ts
@@ -1,50 +1,88 @@
 import { removeChildren } from "./utils"
 
+
+export interface CharGrid {
+    width: () => number,
+    height: () => number,
+
+    setColor: (x: number, y: number, color: string) => void,
+    setText: (x: number, y: number, text: string) => void,
+}
 export class SpanGrid {
     private spans: HTMLSpanElement[][]
 
     constructor(
-        public width: number,
-        public height: number,
-        private container: HTMLElement,
+        public _width: number,
+        public _height: number,
+        private container?: HTMLElement,
 
         deleteChildren: boolean = false
     ) {
-        if (deleteChildren) {
+        if (container && deleteChildren) {
             container.textContent = ''
             removeChildren(container)
         }
 
-
         this.spans = []
 
-        for (let y = 0; y < height; y++) {
+        for (let y = 0; y < _height; y++) {
             const row = []
 
-            for (let x = 0; x < width; x++) {
+            for (let x = 0; x < _width; x++) {
                 const span = document.createElement('span')
                 span.textContent = [':', ')', ' '][Math.floor(Math.random() * 2.9)]
 
                 row.push(span)
-                this.container.appendChild(span)
+                if (this.container) {
+                    this.container.appendChild(span)
+                }
             }
 
-            this.container.appendChild(document.createElement('br'))
+            if (this.container) {
+                this.container.appendChild(document.createElement('br'))
+            }
             this.spans.push(row)
         }
     }
 
+    width() {
+        return this._width
+    }
+    
+    height() {
+        return this._height
+    }
+
     getSpan(x: number, y: number) {
         if (x < 0
-            || x >= this.width
+            || x >= this._width
             || y < 0
-            || y >= this.height
+            || y >= this._height
         ) {
-            console.error({ x, y, width: this.width, height: this.height })
+            console.error({ x, y, width: this._width, height: this._height })
             // throw new Error()
             return undefined
         }
 
         return this.spans[y][x]
     }
+
+    setColor(x: number, y: number, color: string) {
+        const span = this.getSpan(x, y)
+        if (!span) {
+            return
+        }
+        
+        span.style.color = color
+    }
+    
+    setText(x: number, y: number, text: string) {
+        const span = this.getSpan(x, y)
+        if (!span) {
+            return
+        }
+        
+        span.textContent = color
+    }
 }
+
